834923ed3fb70e19f5df4764c7191c636d75e30c
Can3do <juanpablocanedoprincipal@gmail.com>
2024-05-11 13:26:50 -0300

    cambiando el tamaño del titulo en el header

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
 	return (
 		<header className='flex h-16 w-full border-b px-6 justify-center'>
 			<div className='flex items-center justify-between w-full max-w-7xl md:px-16 '>
-				<p className='font-semibold'>Tasks App</p>
+				<p className='font-semibold text-lg'>Tasks App</p>
 
 				<ModeToggle />
 			</div>
